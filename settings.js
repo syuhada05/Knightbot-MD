70c3688372a06a926af75ce8c5a5dbfe29c21bcb
syuhada05 <addastorefollower@gmail.com>
2025-04-18 17:17:40 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ʀʏɪᴢ',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ʀʏɪᴢ Bot",
+  botOwner: 'ʀʏɪᴢ', // Your name
+  ownerNumber: '601139137441', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
